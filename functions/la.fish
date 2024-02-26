dc0379937312e20d7de92fdb2061e751ade413ae
Ken Rachynski <krachynski@ice-edge.com>
2024-02-26 10:44:42 -0700

    detect eza/exa on run

--- functions/la.fish
+++ functions/la.fish
@@ -1,3 +1,7 @@
-function la --wraps='exa' --description 'Displays a list of files in a directory using a long format'
-  exa -alFh --group-directories-first --git --colour=auto $argv
+function la --wraps='exa' --wraps='eza' --description 'Displays a list of files in a directory using a long format'
+	if type --quiet eza
+		eza -Alh --group-directories-first --git --colour=auto $argv
+	else
+		exa -alFh --group-directories-first --git --colour=auto $argv
+	end
 end
