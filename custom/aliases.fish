57c719eda1628e5004f1bf6ad985cc917a4ea772
Ken Rachynski <krachynski@ice-edge.com>
2024-02-26 10:10:35 -0700

    clean up aliases

--- custom/aliases.fish
+++ custom/aliases.fish
@@ -1,15 +1,2 @@
-alias how "howzit"
-alias sr "/usr/local/bin/sd"
-alias lld "ls -ld"
 alias c "clear"
-balias got "git"
-balias gut "git"
-balias gp "git push"
-balias gpl "git pull"
-balias st "git status"
-
-# Made these autoload functions but then they're hidden files...
-# Mo' dots mo' problems
-alias .. "updown .."
-alias ... "updown ../.."
-alias .... "updown ../../.."
+alias k "kubectl"
