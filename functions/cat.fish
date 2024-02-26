3836f3b14fb3d4dee35faa7a7a1343ff3112a687
Ken Rachynski <krachynski@ice-edge.com>
2024-02-26 11:06:39 -0700

    fix up set to use function scoped variables

--- functions/cat.fish
+++ functions/cat.fish
@@ -1,11 +1,11 @@
-function cat -d "Use bat instead of cat unless it's a Markdown file, then use mdless"
+function cat --wraps='bat' --wraps='batcat' -d "Use bat instead of cat unless it's a Markdown file, then use mdless"
 	if not test -f $argv
 		echo "File not found: $argv"
 		return 0
 	end
-	set -l batcmd bat
-	if not type --quiet bat
-		set -l batcmd batcat
+	set -f batcmd bat
+	if not type --quiet $batcmd
+		set -f batcmd batcat
 	end
 	command $batcmd --style plain --theme OneHalfDark $argv
 end
